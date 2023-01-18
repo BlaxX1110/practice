0cbdd0cb07dd04dae71f711992a7a106e85b8362
BlaxX1110 <122962493+BlaxX1110@users.noreply.github.com>
2023-01-18 14:58:56 +0700

    Add files via upload

---
+++ profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2 style="text-align: center"><u>Profile</u></h2>
+    <h4 style="text-align: center"><u>Hai!</u></h2>
+    <p style="text-align: center"><u>Nama saya Raisya Ekan O R</u></p>
+    <p style="text-align: center"><u>Dari kls XI-RPL</u></p>
+    <img src="yes.jpg"></img>
+</body>
+</html>
